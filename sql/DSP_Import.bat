5362ca328b85f2cda9b9406ab344ac12dc1a6d9d
Mark Nelson <phlarebot@gmail.com>
2016-08-20 15:24:08 -0600

    ditching some old SQL files from initial setup

--- sql/DSP_Import.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-ECHO Loading dspdb tables into the database
-cd c:\darkstar\sql
-FOR %%X IN (*.sql) DO ECHO Importing %%X & "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql" dspdb -h localhost -u root -ppurple< %%X
-
-ECHO Finished!
