b0eb0fa765506e7cac62c875267381f51df95f85
Mark Nelson <phlarebot@gmail.com>
2016-08-20 22:39:23 -0600

    more lizzy drop tweaks

--- sql/DSP_nm_tweaks.sql
+++ sql/DSP_nm_tweaks.sql
@@ -1,9 +1,9 @@
 -- Leaping Lizzy 
 
 --  SELECT * FROM mob_droplist WHERE dropId = 2036;
--- add real leaping boots to leaping lizzy
+-- add real leaping boots to leaping lizzy and make fairly decent chance to drop
 INSERT INTO mob_droplist (dropId, type, itemId, rate) 
-SELECT 2036 dropId, 0 type, 13014 itemId, 140 rate
+SELECT 2036 dropId, 0 type, 13014 itemId, 210 rate
 FROM DUAL
 WHERE NOT EXISTS (
 	SELECT 1 
@@ -11,7 +11,7 @@ WHERE NOT EXISTS (
 	WHERE dropId = 2036 AND itemId = 13014
 	LIMIT 1
 );
--- also update bounding boots too be 3x likely to drop
+-- also update bounding boots too be 6x likely to drop
 UPDATE mob_droplist
-SET rate = 210
+SET rate = 420
 WHERE dropId = 2036 AND itemId = 15351;
