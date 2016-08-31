48f7fa3b4e910eb96a831e12f185db8e5cbbb1a9
Mark Nelson <phlarebot@gmail.com>
2016-08-31 14:11:03 -0600

    fixing issue with onMobDeath not being called in Giddeus

--- scripts/zones/Giddeus/mobs/Yagudo_Persecutor.lua
+++ scripts/zones/Giddeus/mobs/Yagudo_Persecutor.lua
@@ -6,17 +6,26 @@
 require("scripts/zones/Giddeus/MobIDs");
 
 -----------------------------------
+-- onMobDeath
+-----------------------------------
+function onMobDeath(mob)
+    -- luautils throwing errors if this isn't here
+end;
+
+-----------------------------------
 -- onMobDespawn
 -----------------------------------
 
 function onMobDespawn(mob)
-
     local mobID = mob:getID();
     if (Juu_Duzu_the_Whirlwind_PH[mobID] ~= nil) then
-
-        local ToD = GetServerVariable("[POP]Juu_Duzu_the_Whirlwind");
         if (ToD <= os.time(t) and GetMobAction(Juu_Duzu_the_Whirlwind) == 0) then
+            -- alerts to phlare on pops
+            -- local player = GetPlayerByName('Phlare');
+            -- player:PrintToPlayer("Juu Duzu the Whirlwind PH despawned.");
+            local ToD = GetServerVariable("[POP]Juu_Duzu_the_Whirlwind");
             if (math.random(1,5) == 3) then
+                -- player:PrintToPlayer("Juu Duzu the Whirlwind will pop.");
                 UpdateNMSpawnPoint(Juu_Duzu_the_Whirlwind);
                 GetMobByID(Juu_Duzu_the_Whirlwind):setRespawnTime(GetMobRespawnTime(mobID));
                 SetServerVariable("[PH]Juu_Duzu_the_Whirlwind", mobID);
